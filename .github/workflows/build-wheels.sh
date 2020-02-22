9980da28b1d25d63b0c1c2c69cf5a738ce670401
Chris Seymour <christopher.seymour@nanoporetech.com>
2020-02-22 13:00:23 +0000

    fix wheel location

--- .github/workflows/build-wheels.sh
+++ .github/workflows/build-wheels.sh
@@ -2,9 +2,15 @@
 set -e -x
 
 for PYBIN in /opt/python/cp3[5678]*/bin; do
+    rm -rf venv3
+    "${PYBIN}/python3" -m venv venv3
+    source venv3/bin/activate
+    python3 -m pip install -r requirements.txt
+    make test
     "${PYBIN}/python3" setup.py bdist_wheel
+    deactivate
 done
 
-for wheel in target/wheels/*.whl; do
+for wheel in dist/*.whl; do
     auditwheel repair "${wheel}"
 done
