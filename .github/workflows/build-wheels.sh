24d037424fde1b0866b35193b872488b0a3ad8bb
Chris Seymour <christopher.seymour@nanoporetech.com>
2020-10-01 21:47:32 +0100

    try cmake==3.18.2

--- .github/workflows/build-wheels.sh
+++ .github/workflows/build-wheels.sh
@@ -5,7 +5,7 @@ for PYBIN in /opt/python/cp3[5678]*/bin; do
     rm -rf venv3
     "${PYBIN}/python3" -m venv venv3
     source venv3/bin/activate
-    python3 -m pip install cmake==3.13.3 
+    python3 -m pip install cmake==3.18.2
     python3 -m pip install -r requirements.txt
     make test
     "${PYBIN}/python3" setup.py bdist_wheel
