61bae13ad1764420f41750083bb51ae8c5ec0686
Chris Seymour <christopher.seymour@nanoporetech.com>
2021-03-22 14:28:15 +0000

    py3.9 support

--- .github/workflows/build-wheels.sh
+++ .github/workflows/build-wheels.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -x
 
-for PYBIN in /opt/python/cp3[5678]*/bin; do
+for PYBIN in /opt/python/cp3[56789]*/bin; do
     rm -rf venv3
     "${PYBIN}/python3" -m venv venv3
     source venv3/bin/activate
