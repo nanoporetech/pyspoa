9bcfb6c50ad699096bbfd684e6e4b46b8197708a
Chris Seymour <christopher.seymour@nanoporetech.com>
2023-04-22 10:45:11 +0100

    pyproject.toml

--- .github/workflows/build-wheels.sh
+++ .github/workflows/build-wheels.sh
@@ -5,10 +5,8 @@ for PYBIN in /opt/python/cp3[67891]*/bin; do
     rm -rf venv3
     "${PYBIN}/python3" -m venv venv3
     source venv3/bin/activate
-    python3 -m pip install cmake==3.18.4
-    python3 -m pip install -r requirements.txt
     make test
-    "${PYBIN}/python3" setup.py bdist_wheel
+    "${PYBIN}/python3" -m pip wheel
     make clean
     deactivate
 done
